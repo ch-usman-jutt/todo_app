994c19844dbe1f04fab66b14d59fa61bcecf855c
Muhammad Usman Ali Nasir <usman.nasir@A006-01420.local>
2024-07-16 21:13:25 +0500

    Return dummy static data in response of calling list endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,10 @@
-from typing import Union
-
+from typing import List
 from fastapi import FastAPI
+from models import Todo
+from database import todos_detail
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}/{item_name}")
-def read_item(item_id: int, item_name: str):
-    return {"item_id": item_id, "name": item_name}
+@app.get("/list", response_model=List[Todo])
+def todos():
+    return todos_detail
